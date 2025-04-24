97c1e66fbf237d5e9462a552868c0bedbdf6f182
Muhammad Ubaid ur Rehman <uurrehman735@gmail.com>
2025-04-24 09:24:38 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+import Portfolio from "./portfolio";
+
+function App() {
+
+  return (
+
+    <div>
+    
+    <Portfolio />
+    </div>
+  );
+}
+
+export default App;
